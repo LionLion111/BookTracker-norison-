3f93c728ea6ec5913cdb9b7d8617c0b9b061ff38
Ihor Matiev <ihor.matiev@gmail.com>
2025-02-15 13:53:39 +0100

    Added open api and scalar documentation

--- src/back-end/BookTracker.Api/Program.cs
+++ src/back-end/BookTracker.Api/Program.cs
@@ -3,15 +3,26 @@ using BookTracker.Persistence;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthorization();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("Database")));
 
 builder.Services.AddIdentityApiEndpoints<IdentityUser>().AddEntityFrameworkStores<AppDbContext>();
 
+builder.Services.AddOpenApi();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapOpenApi();
+app.MapScalarApiReference();
+app.MapIdentityApi<IdentityUser>();
 
 app.Run();
