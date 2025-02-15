a4bf321ef3a947d2cad5276712f5c99cb1e8f1d9
Ihor Matiev <ihor.matiev@gmail.com>
2025-02-15 13:29:26 +0100

    Added connection string and migrations

--- src/back-end/BookTracker.Api/Program.cs
+++ src/back-end/BookTracker.Api/Program.cs
@@ -1,4 +1,14 @@
+using BookTracker.Persistence;
+
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("Database")));
+
+builder.Services.AddIdentityApiEndpoints<IdentityUser>().AddEntityFrameworkStores<AppDbContext>();
 
 var app = builder.Build();
 
